e53bc17abbd015703debdfa94dac3c6c58db6346
Felix Vicenta <fv104@hotmail.com>
2018-11-08 16:57:22 +0100

    Primera version

---
+++ public/src/js/main.js
@@ -0,0 +1,56 @@
+function miOnDragStart(e) {
+    event.dataTransfer.setData('id', e.target.id)
+    console.log('Drag start',e.target.id, ' = ',e.dataTransfer.getData('id'))
+}
+
+function miOnDragOver(e) {
+    e.preventDefault();
+}
+
+function miOnDrop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData('id');
+    e.target.appendChild(document.getElementById(data));
+    console.log('Drag drop', data);
+}
+
+
+function soundcloud () {
+    SC.initialize({
+        client_id: 'unnFdubicpq7RVFFsQucZzduDPQTaCYy'
+    });
+
+    SC.get('/tracks', {
+        q: 'u2'
+    }).then(function (tracks){
+        console.log(tracks)
+        $('#lista').append('<div class="caratula" draggable="true"><img src="' + tracks[1].artwork_url + '" /></div>');
+
+        SC.stream('/tracks/'+tracks[1].id).then(function (player){
+            player.play().then(function(){
+                console.log('Playback started!');
+            }).catch(function(e){
+                console.error('Playback rejected. Try calling play() from a user interaction.', e);
+            });
+        })
+    })
+
+}
+
+
+
+
+        /*SC.stream('/tracks/293').then(function(player){
+            player.play();
+        });*/
+
+
+
+
+
+
+
+
+
+
+
